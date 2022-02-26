8fbb78cb68dd95fa90c3038b9cfcab32d452f4b7
FindYGL <1072348750@qq.com>
2022-02-26 10:22:12 +0800

    增加usb hid的功能

--- Mouse.py
+++ Mouse.py
@@ -1,5 +1,7 @@
 from machine import Pin
 import time
+import hid
+
 
 # 摇杆的端口配置 -> AD采样端口
 deviationX = machine.ADC(3)
@@ -9,14 +11,18 @@ deviationY = machine.ADC(2)
 buttonA = Pin(6, Pin.IN, Pin.PULL_UP)
 buttonB = Pin(5, Pin.IN, Pin.PULL_UP)
 
+# usb_hid 配置
+mouse = hid.Mouse()
+
 while True:
     
 #     adc1 = deviationX.read_u16() - 32768
 #     print(adc1)
 #     adc2 = deviationY.read_u16() - 32768
 #     print(adc2)
-    aValue = buttonA.value()
-    print(aValue)
-    bValue = buttonB.value()
-    print(bValue)
-    time.sleep(0.5)
+#     aValue = buttonA.value()
+#     print(aValue)
+#     bValue = buttonB.value()
+#     print(bValue)
+    mouse.move(50, 0)
+    time.sleep(1)
