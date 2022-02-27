fd3c113c64965601732e8625057985b799a14052
FindYGL <1072348750@qq.com>
2022-02-27 17:18:08 +0800

    实现菜单界面

--- Mouse.py
+++ Mouse.py
@@ -1,4 +1,4 @@
-from machine import Pin
+from machine import Pin,Timer
 import time
 import hid
 import st7789 as st7789
@@ -30,32 +30,55 @@ disp_width = 240 #屏幕宽度
 disp_height = 240 #屏幕高度
 CENTER_Y = int(disp_width/2) #Y轴中心点
 CENTER_X = int(disp_height/2)#X轴中心点
-print(uos.uname())
+# print(uos.uname())
 spi_sck=machine.Pin(2)#SCK
 spi_tx=machine.Pin(3) #TX
 spi0=machine.SPI(0,baudrate=4000000, phase=1, polarity=1, sck=spi_sck, mosi=spi_tx)
-print(spi0)
+# print(spi0)
+
+# 屏幕显示
 display = st7789.ST7789(spi0, disp_width, disp_width,
                           reset=machine.Pin(st7789_res, machine.Pin.OUT),
                           dc=machine.Pin(st7789_dc, machine.Pin.OUT),
                           xstart=0, ystart=0, rotation=0)
 display.fill(st7789.WHITE)
-# display.text(font2, "Hello!", 10, 10)
-# display.text(font2, "RPi Pico", 10, 40)
-# display.text(font2, "MicroPython", 35, 100)
-# display.text(font2, "EETREE", 35, 150)
-# display.text(font2, "www.eetree.cn", 30, 200)
-f_image = open(image_file0, 'rb')
-# mouse_move(CENTER_X, CENTER_Y)
+display.text(font2, "Menu", 90, 10, color=st7789.BLACK, background=st7789.WHITE)
+display.text(font2, "Connect PC", 10, 50, color=st7789.BLACK, background=st7789.WHITE)
+display.text(font2, "Sensitivity", 10, 100, color=st7789.BLACK, background=st7789.WHITE)
+display.text(font2, "Other", 10, 150, color=st7789.BLACK, background=st7789.WHITE)
+currentX = 200
+currentY = 200
+f_image = open("/mouse_pic.bin", 'rb')
+mouse_move(currentX, currentY)
+
+
+# 按键扫描
+# tim = Timer()
+# def key_scan(timer):
+# 
+#     
+#     
+# tim.init(period=100, mode=Timer.PERIODIC, callback=key_scan)#100ms调用一次
+
+status_mode = 0
 
 while True:
-    
     Xdeviation = XdeviationAD.read_u16()
     Ydeviation = YdeviationAD.read_u16()
-       
-#     Xdeviation = int((XdeviationAD.read_u16() - 32768) / 3500)
-#     Ydeviation = int((YdeviationAD.read_u16() - 32768) / 3500)
+    if (Xdeviation > 25000) and (Xdeviation < 35000):
+        Xdeviation = 32500
+    if (Ydeviation > 25000) and (Ydeviation < 35000):
+        Ydeviation = 32500
+    
+    if (Xdeviation == 32500) and (Ydeviation == 32500):
+        continue
+    
+    Xdeviation = Xdeviation - 32500
+    Ydeviation = Ydeviation - 32500
+    
+    
 #     print(Xdeviation)
+#     print(Ydeviation)
 #     if (Xdeviation != 0) or (Ydeviation != 0):
 #         mouse.move(Xdeviation, Ydeviation)
 #     
@@ -64,7 +87,36 @@ while True:
 #     if buttonB.value() == 0:
 #         mouse.click(mouse.BUTTON_LEFT)
 
-    f_image = open(image_file0, 'rb')
-    mouse_move(Xdeviation*10, Ydeviation*10)
-    time.sleep(0.1)
+#     f_image = open(image_file0, 'rb')
+#     mouse_move(Xdeviation*10, Ydeviation*10)
+#     time.sleep(0.1)
+    
+    if (status_mode == 0): #主菜单
+        display.fill_rect(currentX, currentY, 15, 21, st7789.WHITE)
+        display.text(font2, "Menu", 90, 10, color=st7789.BLACK, background=st7789.WHITE)
+        display.text(font2, "Connect PC", 10, 50, color=st7789.BLACK, background=st7789.WHITE)
+        display.text(font2, "Sensitivity", 10, 100, color=st7789.BLACK, background=st7789.WHITE)
+        display.text(font2, "Other", 10, 150, color=st7789.BLACK, background=st7789.WHITE)
+
+        currentX = currentX + int(Xdeviation/800)
+        if (currentX < 0):
+            currentX = 0
+        if (currentX > 239):
+            currentX = 239            
+        currentY = currentY + int(Ydeviation/800)
+        if (currentY < 0):
+            currentY = 0
+        if (currentY > 239):
+            currentY = 239
+            
+        f_image = open("/mouse_pic.bin", 'rb')
+        mouse_move(currentX, currentY)
+
+        
+        
+        
+    # display.text(font2, "www.eetree.cn", 30, 200)
+#     elif (status_mode == 1)
+#     
+#     elif (status_mode == 2)
     
